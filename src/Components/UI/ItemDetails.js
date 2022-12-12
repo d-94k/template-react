f9d6866e0980df8e5bb82362dfdbd6207082e1d3
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-12 12:10:02 +0100

    added link to itemdetails

--- src/Components/UI/ItemDetails.js
+++ src/Components/UI/ItemDetails.js
@@ -1,25 +1,39 @@
 import { Fragment } from "react";
 import { useParams } from "react-router-dom";
-
+import { DATA } from "../../Database";
 const ItemDetails = () => {
+  //const { id, name, price, image, description } = DATA;
   const itemIt = useParams();
   return (
     <Fragment>
-      <h1>{itemIt.id}Details</h1>
+      {/*<h1>{itemIt.id}Details</h1>
       <div>
         <div>
-          <img width="350px" alt="" src={""} />
+          <img width="350px" alt="" src={image} />
         </div>
         <div>
-          <h3 className="">Name: {""}</h3>
+          <h3 className="">Name: {name}</h3>
           <div className="">
-            <strong>Price:</strong> €{""}
+            <strong>Price:</strong> €{price}
           </div>
           <div className="">
             <strong>Description:</strong>
           </div>
         </div>
-      </div>
+      </div> */}
+      {DATA.map((item) => (
+        <div>
+          <div>
+            <img src={item.image} alt="" />
+          </div>
+          <div>
+            <p>{item.name}</p>
+          </div>
+          <div>
+            <p>{item.price}</p>
+          </div>
+        </div>
+      ))}
     </Fragment>
   );
 };
