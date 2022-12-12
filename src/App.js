b8efdf1fdbc96b92b680787982996f7ce36681ee
Meli357 <wyck_f@hotmail.com>
2022-12-12 10:41:04 +0100

    add listItem

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ import Wrapper from "./Components/UI/Wrapper";
 import AddItem from "./Pages/AddItem";
 import { Routes, Route } from 'react-router-dom';
 
-function App() {
 
+
+function App(props) {
+  const [data,setData]= useState(DATA)
+  const newMeth = (newData) => {
+    setData((listItem)=>[...listItem, newData])
+      console.log("this is :", newData)
+  }
+  
   return (
     <div>
-    <Header />
-    <Routes>
-    <Route path="additem" element={<AddItem />} />
-    <Route path="/" element={<div><ul className="slider">
-        <li>slider</li>
-      </ul> <Wrapper>
-      </Wrapper></div>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="additem" element={<AddItem onSaveItem={newMeth}/>} />
+        <Route path="/" element={<div><ul className="slider">
+          <li>slider</li>
+        </ul> <Wrapper>{data.map((item) => 
+          <Items key={item.id} name={item.name} price={item.price} image={item.image} descsription={item.description} />
+        )}
+          </Wrapper></div>} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/AddItem.js
+++ src/Pages/AddItem.js
@@ -2,14 +2,15 @@ import { useState } from "react";
 import Form from "../Components/UI/Form";
 import { DATA } from "../Database";
 
-const AddItem = () => {
+const AddItem = (props) => {
 
     const onSaveItem = (interval) => {
         const item = {
             id: Math.random(),
             ...interval
         }
-        console.log(item);
+        // console.log(item);
+        props.onSaveItem(item)
     }
     return (
         <Form render={onSaveItem} />
